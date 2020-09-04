b5f30c1b5216321c0fb14f626945fc4e24be86ee
CodyC <codyicastro@gmail.com>
2020-09-04 11:45:05 -0400

    animated title

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
+
 document.addEventListener("DOMContentLoaded", function (event) {
-    const costumeFetchAdapter = new Fetch
+    // const costumeFetchAdapter = new Fetch
+
+
+    // Wrap every letter in a span
+var textWrapper = document.querySelector('.ml3');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml3 .letter',
+    opacity: [0,1],
+    easing: "easeInOutQuad",
+    duration: 2250,
+    delay: (el, i) => 150 * (i+1)
+  }).add({
+    targets: '.ml3',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
+
+
+
 })
 
 class FetchAdapter{
@@ -11,4 +35,5 @@ class FetchAdapter{
         .then(response => response.json())
         .then(costumeData => console.log(costumeData))
     }
+
 }
